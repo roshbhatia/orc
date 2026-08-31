88026210d76a8c392b25b6938db233f7dc72a834
Roshan Bhatia <rshnbhatia@gmail.com>
2026-08-31 15:17:29 -0500

    Initial commit

---
+++ package.nix
@@ -0,0 +1,31 @@
+{
+  biome,
+  bun2nix,
+  stdenv,
+}:
+bun2nix.mkDerivation {
+  pname = "example";
+  version = "0.1.0";
+  src = ./.;
+  module = "src/main.ts";
+
+  nativeBuildInputs = [ biome ];
+  bunDeps = bun2nix.fetchBunDeps { bunNix = ./bun.nix; };
+  bunInstallFlags =
+    if stdenv.hostPlatform.isDarwin then
+      [
+        "--linker=hoisted"
+        "--backend=copyfile"
+      ]
+    else
+      [ "--linker=hoisted" ];
+
+  doCheck = true;
+  checkPhase = ''
+    runHook preCheck
+    biome check .
+    bun run typecheck
+    bun test
+    runHook postCheck
+  '';
+}
