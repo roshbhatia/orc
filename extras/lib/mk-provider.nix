911b5fb10fcf5517b049322fe5065d1d8eaad381
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-06 15:39:54 -0700

    chore(release): prepare 0.11.1

--- extras/lib/mk-provider.nix
+++ extras/lib/mk-provider.nix
@@ -18,7 +18,7 @@ let
   providerRuntimeInputs = [ bash ] ++ runtimeInputs;
   adapter = stdenvNoCC.mkDerivation {
     pname = "orc-provider-${name}-adapter";
-    version = "0.11.0";
+    version = "0.11.1";
     dontUnpack = true;
     strictDeps = true;
 
